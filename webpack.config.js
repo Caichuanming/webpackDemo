100348e448bd480e20522ea0a85abefc67dcccdb
derek_cai <derek_cai@163.com>
2018-04-15 21:50:29 +0800

    增加webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
         path: __dirname + "/build",
         filename: "bundle.js",
     },
+    devServer: {
+        contentBase: "./build",//本地服务器所加载的页面所在的目录
+        historyApiFallback: true,//不跳转
+        inline: true//实时刷新
+    },
     loader: {
         rules: [
             {
