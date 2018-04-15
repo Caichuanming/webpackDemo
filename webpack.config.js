996e916303247162810683c92dab672f689d323a
derek_cai <derek_cai@163.com>
2018-04-15 21:45:56 +0800

    增加babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-    entry: {},
-    output: {},
-    loader: {},
-    module: {},
-    plugins: {},
+    entry: __dirname + "/src/main.js",
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js",
+    },
+    loader: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+            }
+        ],
+    },
+    // module: {},
+    // plugins: {},
 }
