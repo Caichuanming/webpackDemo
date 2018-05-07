c4f9e2bc63540baa1290ef691bbcf93c7b5b1c7c
chuanming.cai <chuanming.cai@mtime.com>
2018-05-07 19:00:40 +0800

    server配置

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import "./index.css";
 // import Promise from "bluebird";
 
-var Promise = require("bluebird");
-const a = new Promise((resolved, rejected) => {
-    if (true) {
-        resolved();
-    } else {
-        rejected();
-    }
-});
+// var Promise = require("bluebird");
+// const a = new Promise((resolved, rejected) => {
+//     if (true) {
+//         resolved();
+//     } else {
+//         rejected();
+//     }
+// });
 // console.log(a);
